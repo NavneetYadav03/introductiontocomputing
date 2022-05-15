bcd7238d4471a3c84cf8fc16b86ef1d6741941d3
NavneetYadav03 <103272349+NavneetYadav03@users.noreply.github.com>
2022-05-15 13:20:13 -0500

    Add files via upload

---
+++ assignment_1_21102061_Lab/ques_1_avg,_of_numbers.py
@@ -0,0 +1,6 @@
+#1.Write a Python program to find average of three numbers entered by the user.
+
+a=int(input("Enter first number "))
+b=int(input("Enter second number "))
+c=int(input("Enter third number "))
+print("Average of three numbers is ",(a+b+c)/3)
